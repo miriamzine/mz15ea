4c4a40267521930d405f822fb7876053aafc0a62
miriam zine <32112289+miriamzine@users.noreply.github.com>
2017-11-23 18:57:21 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+
+var userNum ="";
+
+function getNum(){
+
+  var getNum = document.getElementById('input').value;
+
+
+  for(i=0;i<getNum;i++){
+
+
+    console.log(i)
+
+
+    document.getElementById('output').innerHTML += i+"\n";
+  }
+}
